86576d479174a390f092e46f6077b49ad0e776a5
Enrico Wegner <wegner1996@gmail.com>
2022-04-03 18:36:43 +0200

    refactoring laplace and SIR

--- src/sampling/laplace.jl
+++ src/sampling/laplace.jl
@@ -25,33 +25,30 @@ function _rand!(rng::AbstractRNG, s::LaplaceApproximation, x::AbstractVector{<:R
     x .= rand(rng, StatsBase.sample(dists, w))
 end
 
-
-function laplace(bnn::BNN, maxiter::Int, ϵ::Float64=0.01; init_θ = randn(bnn.totparams), 
-                 showprogress=true, opt = Flux.RMSProp(), diag = false)
-   
-    θ, conv = find_mode(bnn, maxiter, ϵ; init_θ = init_θ, showprogress = showprogress, opt = opt, verbose = false)
+function laplace(lpdf, initθ::AbstractVector, maxiter::Int, ϵ::Float64=0.01; diag = false, kwargs...) 
+    θ, conv = find_mode(lpdf, initθ, maxiter, ϵ; verbose = false, kwargs...)
     hessian = diag ? Zygote.diaghessian : Zygote.hessian
-    H = hessian(θ -> lp(bnn, θ), θ)
-    Hinv = diag ? 1 ./ H[1] : H\I 
+    H = hessian(θ -> lpdf(θ), θ)
+    Hinv = diag ? 1 ./ -H[1] : (-H)\I 
     if !diag && !LinearAlgebra.isposdef(Hinv)
         error("Hessian was not positive definite. Try using a diagonal approximation")
     end
 
-    # weight = diag ? sqrt(abs(det(diagm(Hinv)))) : sqrt(abs(det(Hinv)))
-    # weight *= lp(bnn, θ)
+    if any(isnan.(θ)) error("Produced NaN during mode finding.") end
+
     weight = 1.0
 
-    return MvNormal(θ, Hinv), weight, conv
+    return MvNormal(θ, Hinv), weight, conv 
 end
 
-function laplace(bnn::BNN, maxiter::Int, M::Int, args...; kwargs...)
+function laplace(lpdf, initθ_distribution::D, maxiter::Int, M::Int, args...; kwargs...) where {D<:Distributions.Distribution}
     if Threads.nthreads() == 1
         @warn "Only one thread Available. Will not work in parallel."
     end
     dists = Array{Distributions.MultivariateNormal}(undef, M)
     weights = ones(M)
     conv = Array{Bool}(undef, M)
-    starting_θ = 10 .* rand(bnn.totparams, M) .- 5
+    starting_θ = rand(initθ_distribution, M) 
 
     p = Progress(M)
     update!(p, 0)
@@ -59,31 +56,50 @@ function laplace(bnn::BNN, maxiter::Int, M::Int, args...; kwargs...)
     l = Threads.SpinLock()
 
     Threads.@threads for m=1:M 
-        dists[m], weights[m], conv[m] = laplace(bnn, maxiter, args...; init_θ = starting_θ[:, m], showprogress = false, kwargs...)
+        dists[m], weights[m], conv[m] = laplace(lpdf, starting_θ[:, m], maxiter, args...; showprogress = false, kwargs...)
         Threads.atomic_add!(jj, 1)
         Threads.lock(l)
         update!(p, jj[])
         Threads.unlock(l)
     end
-    return LaplaceApproximation(dists, StatsBase.ProbabilityWeights(weights ./ sum(weights)), conv)
+    return LaplaceApproximation(dists, StatsBase.ProbabilityWeights(weights ./ sum(weights)), conv) 
+end
+
+function laplace(bnn::BNN, maxiter::Int, ϵ::Float64=0.01; init_θ = randn(bnn.totparams), 
+                diag = false, kwargs...)
+    return laplace(θ -> lp(bnn, θ), init_θ, maxiter, ϵ; diag = diag, kwargs...)
+end
+
+function laplace(bnn::BNN, maxiter::Int, M::Int, args...; kwargs...)
+    return laplace(θ -> lp(bnn, θ), MvNormal(zeros(bnn.totparams), 10*ones(bnn.totparams)), maxiter, M, args...; kwargs...)
+end
+
+function mixture_dens(dists::Vector{D}, w::AbstractVector, θ::AbstractVector) where {D<:Distributions.MultivariateDistribution}
+    dens = zero(eltype(θ))
+    for (di,wi) in zip(dists, w)
+        dens += wi*pdf(di, θ)
+    end
+    return dens
+end
+
+function SIR_laplace(lpdf, la::LaplaceApproximation, n::Int, k::Int)
+    # Sampling Importance Resampling of Laplace approximation
+    sum(la.c) == 0 && error("No laplace approximation converged. Consider running them for longer.")
+    !all(la.c) && @warn "Not all laplace approximation converged. Will only used converged ones." 
+
+    proposal = Distributions.MixtureModel(MvNormal[la.dists...], la.w)
+    initial_samples = rand(proposal, n)
+    initial_samples = collect(eachcol(initial_samples))
+    weights = exp.(lpdf.(initial_samples)) ./ pdf(proposal, initial_samples)
+    choose = StatsBase.sample(1:n, StatsBase.ProbabilityWeights(weights), k; replace = false)
+    chosen_weights = weights[choose]
+    chosen_samples = initial_samples[choose]
+    @info "Mean of importance weights (should be close to one): $(mean(weights))"
+    @info "Mean of importance weights of chosen samples: $(mean(chosen_weights))"
+
+    return hcat(chosen_samples...)
 end
 
 function SIR_laplace(bnn::BNN, la::LaplaceApproximation, n::Int, k::Int)
-    # Sampling Importance Resampling of Laplace approximation
-    sum(la.c) == 0 && error("No laplace approximation converged. Consider running them for longer.")
-    !all(la.c) && @warn "Not all laplace approximation converged. Will only used converged ones."
-
-    dists = la.dists[la.c]
-
-    initial_samples = ones(bnn.totparams, n)
-    weights = ones(n)
-    Threads.@threads for i=1:n 
-        d = StatsBase.sample(dists)
-        s = rand(d)
-        w = exp(lp(bnn, s) - logpdf(d, s))
-        initial_samples[:,i] = s
-        weights[i] = w 
-    end
-
-    return StatsBase.sample(collect(eachcol(initial_samples)), StatsBase.ProbabilityWeights(weights), k; replace = false)
+    return SIR_laplace(θ -> lp(bnn, θ), la, n, k)
 end
