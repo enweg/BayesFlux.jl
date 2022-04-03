7fcce97d455fcb72eb7e98885f60e04b5a325971
Enrico Wegner <wegner1996@gmail.com>
2022-04-03 17:33:48 +0200

    refactoring mode finding

--- src/optimise/modes.jl
+++ src/optimise/modes.jl
@@ -1,14 +1,17 @@
 # Find the map estimate (mode of the posteior)
 using ProgressBars
 
-function find_mode(bnn::BNN, maxiter::Int, ϵ::Float64=0.01; init_θ = randn(bnn.totparams), 
-             showprogress = true, opt = Flux.RMSProp(), verbose = true)
+function find_mode(bnn::BNN, maxiter::Int, ϵ::Float64=0.01; init_θ = randn(bnn.totparams), kwargs...)
+    return find_mode(θ -> lp(bnn, θ), init_θ, maxiter, ϵ; kwargs...)
+end
 
+function find_mode(lpdf, initθ::AbstractVector, maxiter::Int, ϵ::Float64=0.01; 
+                    showprogress = true, opt = Flux.RMSProp(), verbose = true)
     iterator = showprogress ? ProgressBar(1:maxiter) : 1:maxiter
-    θ = init_θ
-    lj = lp(bnn, θ)
+    θ = copy(initθ)
+    lj = lpdf(θ)
     for t in iterator
-        lj_, g = Zygote.withgradient(θ -> lp(bnn, θ), θ)
+        lj_, g = Zygote.withgradient(θ -> lpdf(θ), θ)
         Flux.update!(opt, θ, -g[1])
         if 1 < (lj_ / lj) < (1+ϵ)
             if (verbose) @info "Found mode in $t iterations." end
@@ -17,6 +20,5 @@ function find_mode(bnn::BNN, maxiter::Int, ϵ::Float64=0.01; init_θ = randn(bnn
         lj = lj_
     end
     if (verbose) @info "Did not converge within $maxiter iterations." end
-    return θ, false
+    return θ, false 
 end
-
