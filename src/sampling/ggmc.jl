1451e73bd9b13fd5e9c09160b579f2b50582cb65
Enrico Wegner <wegner1996@gmail.com>
2022-04-17 16:57:06 +0200

    ggmc

--- src/sampling/ggmc.jl
+++ src/sampling/ggmc.jl
@@ -10,7 +10,11 @@ K(m, M) = 1/2 * m' * inv(M) * m
 
 function ggmc(llike::Function, lpriorθ::Function, batchsize::Int, y::Vector{T}, x::Union{Vector{Matrix{T}}, Matrix{T}}, 
               initθ::Vector{T}, maxiter::Int;
-              M = diagm(ones(length(initθ))), h = 0.2, γ = 1.5, temp = 1) where {T <: Real}
+              M = diagm(ones(length(initθ))), l = 0.01, β = 0.5, temp = 1, keep_every = 10) where {T <: Real}
+    
+
+    h = sqrt(l / length(y))
+    γ = -sqrt(length(y)/l)*log(β)
     
     θ = copy(initθ)
     if mod(length(y), batchsize) != 0
@@ -18,11 +22,14 @@ function ggmc(llike::Function, lpriorθ::Function, batchsize::Int, y::Vector{T},
     end
     num_batches = floor(Int64, length(y)/batchsize)
     @info "Using $num_batches batches."
-    θprop = zeros(length(θ), num_batches) 
+    # θprop = zeros(length(θ), num_batches) 
+    θprop = copy(θ)
+    tot_iters = maxiter*num_batches
+
 
     p = Progress(maxiter * num_batches, 1, "GGMC ...")
 
-    samples = zeros(eltype(initθ), length(initθ), maxiter*num_batches + 1) 
+    samples = zeros(eltype(initθ), length(initθ), tot_iters + 1) 
     samples[:, 1] = θ
 
     momentum14 = 0.0*similar(θ)
@@ -35,69 +42,137 @@ function ggmc(llike::Function, lpriorθ::Function, batchsize::Int, y::Vector{T},
 
     lMH = -U(θ, llike, lpriorθ, y, x) 
     lastθi = 1 # column of last added samples
-    hastings = zeros(maxiter)
+    hastings = zeros(tot_iters+1)
+    momenta = zeros(length(θ), tot_iters+1)
     for t=1:maxiter
         yshuffel, xshuffel = sgd_shuffle(y, x) 
         for b=1:num_batches 
+            # print("θ = $θ, lp = $(U(θ, llike, lpriorθ, y, x)), K = $(K(momentum, M))")
+            # readline()
             xbatch = sgd_x_batch(xshuffel, b, batchsize)
             ybatch = sgd_y_batch(yshuffel, b, batchsize)
-            # g = (length(y)/batchsize) * Zygote.gradient(θ -> llike(θ, ybatch, xbatch) + lpriorθ(θ), θ)[1]
+            # printstyled("#### 11111111111\n", color = :light_magenta)
+            # println("θ = $θ")
             g = (length(y)/batchsize) * Zygote.gradient(θ -> U(θ, llike, lpriorθ, ybatch, xbatch), θ)[1]
+            # println("g = $g")
+            # println("Ufull = $(U(θ, llike, lpriorθ, y, x))")
+            # println("Ubatch = $(U(θ, llike, lpriorθ, ybatch, xbatch))")
+            # println("ybatch = $ybatch")
+            # println("xbatch = $xbatch")
             if any(isnan.(g))
                 println(θ)
-                error("NaN in gradient. This is likely due to a too large step size. Try using different parameter settings")
+                # error("NaN in gradient. This is likely due to a too large step size. Try using different parameter settings")
+                @warn "NaN in gradient. Will turn back to old θ"
+                return samples[:, 1:lastθi], hastings[1:lastθi], momenta[:, 1:((t-1)*num_batches + b)]
+                if mod((t-1)*num_batches + b, keep_every) == 0
+                    samples[:, lastθi+1] = samples[:, lastθi-1]
+                    θ = samples[:, lastθi-1]
+                    lastθi += 1
+                end 
+                continue
             end
 
-            if lastθi > 2 & (t-1)*num_batches + b < 1000
+            adapruns = 2000
+            if lastθi > 100 && lastθi <= adapruns/2 && mod((t-1)*num_batches+b, keep_every) == 0
                 # doing very simple adaptation 
                 # TODO: check literature for better way
                 α = 0.0
-                Madap = diagm(1 ./ vec(var(samples; dims = 2)))
+                Madap = diagm(1 ./ sqrt.(vec(var(samples; dims = 2))))
                 M = α*M + (1-α)*Madap
                 # println(M)
                 # Mhalf = cholesky(M).L
                 Mhalf = sqrt.(M)
+                lastθi == Int(adapruns/2) && println("final M = $(diag(M))")
             end
 
 
             momentum14 .= sqrt(a)*momentum .+ sqrt((1-a)*temp)*Mhalf*randn(length(θ))
             momentum12 .= momentum14 .- h/2 * g
+            # println("m14 = $momentum14")
+            # println("m12 = $momentum12")
             # println("gradient before update: $g")
             θ .= θ .+ h*inv(M)*momentum12
-            θprop[:,b] = θ
+            θprop = θ
 
-            # g = (length(y)/batchsize) * Zygote.gradient(θ -> llike(θ, ybatch, xbatch) + lpriorθ(θ), θ)[1]
+            # printstyled("#### 22222222222222222\n", color = :light_magenta)
+            # println("θ = $θ")
             g = (length(y)/batchsize) * Zygote.gradient(θ -> U(θ, llike, lpriorθ, ybatch, xbatch), θ)[1]
+            # println("g = $g")
+            # println("Ufull = $(U(θ, llike, lpriorθ, y, x))")
+            # println("Ubatch = $(U(θ, llike, lpriorθ, ybatch, xbatch))")
+            # println("ybatch = $ybatch")
+            # println("xbatch = $xbatch")
             if any(isnan.(g))
                 println(θ)
-                error("NaN in gradient. This is likely due to a too large step size. Try using different parameter settings")
+                # println(hastings[1:lastθi])
+                # error("NaN in gradient 2. This is likely due to a too large step size. Try using different parameter settings")
+                @warn "NaN in gradient. Will turn back to old θ"
+                return samples[:, 1:lastθi], hastings[1:lastθi], momenta[:, 1:((t-1)*num_batches + b)]
+                if mod((t-1)*num_batches + b, keep_every) == 0
+                    samples[:, lastθi+1] = samples[:, lastθi-1]
+                    θ = samples[:, lastθi-1]
+                    lastθi += 1
+                end 
+                continue
             end
             momentum34 .= momentum12 .- h/2 * g 
             momentum .= sqrt(a)*momentum34 .+ sqrt((1-a)*temp)*Mhalf*randn(length(θ))
+            momenta[:, (t-1)*num_batches + b] = momentum
 
             lMH += K(momentum34, M) - K(momentum14, M)
 
-            update!(p, (t-1)*num_batches + b, showvalues = [(:arate, naccepts/((t-1)*num_batches + b)), 
-                                                            (:iter, (t-1)*num_batches + b)])
+            if mod((t-1)*num_batches + b, keep_every) == 0
+                # time to evaluate MH ratio and keep last draw or start new 
+                lMH += U(θ, llike, lpriorθ, y, x)
+                lMH /= -temp
+                r = rand()
+                hastings[lastθi+1] = exp(lMH)
+                if r < exp(lMH)
+                    samples[:, lastθi+1] .= θprop
+                    naccepts += 1 
+                else 
+                    samples[:, lastθi+1] .= samples[:, lastθi] 
+                    θ = samples[:, lastθi]
+                end
+                lastθi += 1
+
+                if  lastθi <= adapruns
+                    # adapting step size/learning rate
+                    arate = min(1, hastings[lastθi])
+                    H = 0.65 - arate 
+                    eta = lastθi^(-0.5)
+                    h = exp(log(h) - eta*H)
+                    # println(h)
+
+                    lastθi == adapruns && @info "Final l=$(length(y)*h^2)"
+                end
+
+                lMH = -U(θ, llike, lpriorθ, y, x) 
+
+            end
+
+            update!(p, (t-1)*num_batches + b, showvalues = [(:arate, naccepts/lastθi), 
+                                                            (:iter, (t-1)*num_batches + b), 
+                                                            (:samples, lastθi)])
         end
 
-        lMH += U(θ, llike, lpriorθ, y, x)
-        lMH /= -temp
-        r = rand()
-        hastings[t] = exp(lMH)
-        if r < exp(lMH)
-            samples[:, lastθi+1:lastθi+num_batches] .= θprop
-            naccepts += num_batches
-        else 
-            samples[:, lastθi+1:lastθi+num_batches] .= samples[:, lastθi] 
-            θ = samples[:, lastθi]
-        end
-        lastθi += num_batches
-        lMH = -U(θ, llike, lpriorθ, y, x) 
+        # lMH += U(θ, llike, lpriorθ, y, x)
+        # lMH /= -temp
+        # r = rand()
+        # hastings[t] = exp(lMH)
+        # if r < exp(lMH)
+        #     samples[:, lastθi+1:lastθi+num_batches] .= θprop
+        #     naccepts += num_batches
+        # else 
+        #     samples[:, lastθi+1:lastθi+num_batches] .= samples[:, lastθi] 
+        #     θ = samples[:, lastθi]
+        # end
+        # lastθi += num_batches
+        # lMH = -U(θ, llike, lpriorθ, y, x) 
     end
 
     @info "Acceptance Rate: $(naccepts/size(samples,2))"
-    return samples, hastings
+    return samples[:, 1:lastθi], hastings[1:lastθi], momenta
 
 end
 
