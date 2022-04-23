c756345b2a2ce477f03299a728cfcd72b70191e8
Enrico Wegner <wegner1996@gmail.com>
2022-04-23 16:43:23 +0200

    adding multiple chains support

--- src/model/posterior.jl
+++ src/model/posterior.jl
@@ -3,3 +3,9 @@
 function posterior_predict(bnn::BNN, samples::Matrix{T}; kwargs ...) where {T<:Real} 
     return predict(bnn, bnn.loglikelihood, samples; kwargs...)
 end
+
+function posterior_predict(bnn::BNN, samples::Array{T, 3}; kwargs...) where {T<:Real}
+    ppreds = [posterior_predict(bnn, samples[:,:,i]; kwargs...) for i=1:size(samples, 3)]
+    ppred = cat(ppred...; dims = 3)
+    return ppred
+end
