0fda1aaa455d5a9d2d1a11bea34aff14b68c1ecb
Enrico Wegner <wegner1996@gmail.com>
2022-04-23 16:53:27 +0200

    correcting

--- src/model/posterior.jl
+++ src/model/posterior.jl
@@ -6,6 +6,6 @@ end
 
 function posterior_predict(bnn::BNN, samples::Array{T, 3}; kwargs...) where {T<:Real}
     ppreds = [posterior_predict(bnn, samples[:,:,i]; kwargs...) for i=1:size(samples, 3)]
-    ppred = cat(ppred...; dims = 3)
-    return ppred
+    ppreds = cat(ppreds...; dims = 3)
+    return ppreds
 end
