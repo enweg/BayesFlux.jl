2369f1e6609ba5050a2314b04cb541b9360c4ebd
Enrico Wegner <wegner1996@gmail.com>
2022-05-02 19:43:52 +0200

    adding docstrings

--- src/simulations/AR.jl
+++ src/simulations/AR.jl
@@ -2,6 +2,19 @@
 ################################################################################
 # Simulation and construction of AR(p) model
 ################################################################################
+
+"""
+    AR{T<:Real, D<:Distribution, R<:Random.AbstractRNG}
+
+Create an AR(p) model. 
+
+# Fields
+- `Φ`: Coefficient vector for lags. 
+- `burnin`: Burnin to be used when simulating AR(p) model 
+- `e_dist`: Distribution of errors 
+- `rng`: Random Number Generator
+- `seed`: Seed to be used
+"""
 struct AR{T<:Real, D<:Distribution, R<:Random.AbstractRNG}
     Φ::Vector{T} # lag Vector
     burnin::Int
@@ -13,6 +26,17 @@ end
 # Note that the normal is parameterised by the SD not VAR
 AR(Φ::Vector; burnin = 1000, e_dist = Normal(0, 1), rng = Random.GLOBAL_RNG) = AR(Φ, burnin, e_dist, rng, rng == Random.GLOBAL_RNG ? 0 : Int(rng.seed[1]))
 
+"""
+    (mod::AR)(;N=100, starting_values = zeros(size(mod.Φ)[1]), reseed = false)
+
+Simulate an AR(p) model 
+
+# Arguments 
+- `N=100`: Number of time periods to simulate. 
+- `starting_values=zeros(size(mod.Φ)[1])`: Values from which to start simulation 
+- `reseed=false`: Whether to reset the random number generator. 
+
+"""
 function (mod::AR)(;N=100, starting_values = zeros(size(mod.Φ)[1]), reseed = false)
     # Starting the random number genrator from the beginning again? 
     if reseed
@@ -29,6 +53,16 @@ function (mod::AR)(;N=100, starting_values = zeros(size(mod.Φ)[1]), reseed = fa
     return y[(end-N+1):end] 
 end
 
+"""
+    theoretical_dist(mod::AR, data::Vector{Float64})
+
+Get the theoretical mean and variance at each point in time. 
+
+# Arguments 
+- `mod`: an AR(p) model. 
+- `data`: simulated data of the AR(p) model. 
+
+"""
 function theoretical_dist(mod::AR, data::Vector{Float64})
     # This return the theoretical distribution at each point t given 
     # information up until time t-1 
--- src/utils/rnn_utils.jl
+++ src/utils/rnn_utils.jl
@@ -1,3 +1,15 @@
+"""
+    to_RNN_format(sequences::Vector{Matrix{T}}) 
+
+Transform a data into a format used for Recurrent Networks 
+
+Recurrent Networks need a Vecotor of Matrices where each matrix has the format of 
+`features` x `num_sequences` and the vector has length `len_seq`. 
+
+# Arguments
+- `sequences`: A Vecotor of Matrices where each matrix is of the form `n_features` x `len_seq`
+
+"""
 function to_RNN_format(sequences::Vector{Matrix{T}}) where {T}
     # Assumes that each matrix is of the format n_features x len_seq 
     # We need a vector of matrices of format n_featurs x n_seq for RNN layers 
@@ -10,6 +22,13 @@ function to_RNN_format(sequences::Vector{Matrix{T}}) where {T}
     return out
 end
 
+"""
+    to_RNN_format(tensor::Array{T, 3})
+
+Transform a tensor of data into a format used by Recurrent Networks
+
+This function is usually only needed for BFluxR
+"""
 function to_RNN_format(tensor::Array{T, 3}) where {T}
     # R cannot handle vectors of matrices, so R will communicate using tensors
     # these must be retransformed into a vector of sequences. 
