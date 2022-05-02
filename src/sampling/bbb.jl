da9793493c095963bd49306a3c1a24aa7644fc1f
Enrico Wegner <wegner1996@gmail.com>
2022-05-02 20:14:26 +0200

    moving step outside of loop

--- src/sampling/bbb.jl
+++ src/sampling/bbb.jl
@@ -3,9 +3,35 @@
 
 using ProgressMeter
 
+"""
+    getθ(ψ, ϵ, get_μ_Σ)
+
+Get θ, the network parameters.
+
+Given a vector ψ corresponding to the reparameterised variational distribution
+and a vector ϵ, obtain the mean, μ, and the variance, Σ, using `get_μ_Σ` and 
+calculate the network parameters, θ, as θ = μ + Σϵ ~ Normal(μ, Σ'Σ)
+
+# Arguments
+- `ψ`: Vector of parameters for the variational family. Usually contains both μ and Σ in vector form 
+- `ϵ`: A random draw form the reparameterised variational family. This is usually a standard (multivariate)normal
+- `get_μ_Σ`: A function that takes ψ and returns μ and Σ. 
+
+"""
 function getθ(ψ, ϵ, get_μ_Σ)
     μ, Σ = get_μ_Σ(ψ)
     return μ .+ Σ * ϵ
+end
+
+
+function bbb_step!(ψ, ybatch, xbatch, nsamples, nparams, bbb_objective, opt)
+    g = zeros(eltype(ψ), size(ψ, 1), nsamples)
+    for s=1:nsamples
+        ϵ = randn(nparams)
+        g[:, s] = Zygote.gradient(ψ -> bbb_objective(ψ, ϵ, ybatch, xbatch), ψ)[1]
+    end
+    g = mean(g; dims = 2)
+    Flux.update!(opt, ψ, g)
 end
 
 function bbb(llike, lpriorθ, initψ::Vector{T}, get_μ_Σ, nsamples::Int, maxiter::Int, 
@@ -13,6 +39,8 @@ function bbb(llike, lpriorθ, initψ::Vector{T}, get_μ_Σ, nsamples::Int, maxit
              opt = Flux.RMSProp(), windowlength = 100, showprogress = true, 
              p = Progress(maxiter*floor(Int, length(y)/batchsize); dt = 1, desc = "Bayes by Backprop ...", enabled = showprogress)) where {T<:Real}
 
+    ψ = copy(initψ)
+    nparams = length(get_μ_Σ(ψ)[1])
     nbatches = floor(Int, length(y)/batchsize)
     if mod(length(y), batchsize) != 0
         @warn "length(y) is not a multiple of batchsize. Discarding some observations each iteration."
@@ -24,10 +52,7 @@ function bbb(llike, lpriorθ, initψ::Vector{T}, get_μ_Σ, nsamples::Int, maxit
     Losses will be calculated using a window length of $windowlength. 
     """
 
-    ψ = copy(initψ)
-    bbb_kl(ψ, ϵ, ybatch, xbatch) = logpdf(MvNormal(get_μ_Σ(ψ)[1], Symmetric(get_μ_Σ(ψ)[2]*get_μ_Σ(ψ)[2]')), getθ(ψ, ϵ, get_μ_Σ)) - nbatches * llike(getθ(ψ, ϵ, get_μ_Σ), ybatch, xbatch) - lpriorθ(getθ(ψ, ϵ, get_μ_Σ))
-
-    nparams = length(get_μ_Σ(ψ)[1])
+    bbb_objective(ψ, ϵ, ybatch, xbatch) = logpdf(MvNormal(get_μ_Σ(ψ)[1], Symmetric(get_μ_Σ(ψ)[2]*get_μ_Σ(ψ)[2]')), getθ(ψ, ϵ, get_μ_Σ)) - nbatches * llike(getθ(ψ, ϵ, get_μ_Σ), ybatch, xbatch) - lpriorθ(getθ(ψ, ϵ, get_μ_Σ))
 
     ravalues = zeros(windowlength)
     losses = zeros(maxiter-windowlength)
@@ -37,28 +62,18 @@ function bbb(llike, lpriorθ, initψ::Vector{T}, get_μ_Σ, nsamples::Int, maxit
         for b=1:nbatches
             ybatch = sgd_y_batch(yshuffle, b, batchsize)
             xbatch = sgd_x_batch(xshuffle, b, batchsize)
-
-            g = zeros(eltype(ψ), size(ψ)[1], nsamples)
-            for s=1:nsamples
-                ϵ = randn(nparams)
-                g[:, s] = Zygote.gradient(ψ -> bbb_kl(ψ, ϵ, ybatch, xbatch), ψ)[1]
-            end
-            g = mean(g; dims = 2)
-            # ϵ = rand(MvNormal(zeros(nparams), ones(nparams)), nsamples)
-            # g = Zygote.gradient(ψ -> mean(bbb_kl.([ψ], eachcol(ϵ), [ybatch], [xbatch])), ψ)[1]
-            Flux.update!(opt, ψ, g)
-            # update!(p, (i-1)*nbatches + b)
+            bbb_step!(ψ, ybatch, xbatch, nsamples, nparams, bbb_objective, opt)
             next!(p)
         end
 
+        # Calculating the moving average of the loss
         ϵ = randn(nparams)
-        loss = bbb_kl(ψ, ϵ, y, x)
+        loss = bbb_objective(ψ, ϵ, y, x)
         raindex = mod(i, windowlength) + 1
         ravalues[raindex] = loss
         if i > windowlength
             losses[i-windowlength] = mean(ravalues)
         end
-
     end
 
     return MvNormal(get_μ_Σ(ψ)[1], Symmetric(get_μ_Σ(ψ)[2]*get_μ_Σ(ψ)[2]')), initψ, ψ, losses
