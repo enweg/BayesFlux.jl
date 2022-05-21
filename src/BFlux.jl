d2b27f5825e6ad8b9668463c704d4a7b1bfeb453
Enrico Wegner <wegner1996@gmail.com>
2022-05-22 01:01:55 +0200

    restructuring

--- src/BFlux.jl
+++ src/BFlux.jl
@@ -1,17 +1,29 @@
 module BFlux
 
 include("./model/BNN.jl")
+export BNN
+export split_params
+export loglikeprior, ∇loglikeprior
+
 include("./model/deconstruct.jl")
+export destruct
 
 include("./likelihoods/abstract.jl")
 include("./likelihoods/feedforward.jl")
 include("./likelihoods/seq_to_one.jl")
+export BNNLikelihood, predict
+export FeedforwardNormal, FeedforwardTDist
+export SeqToOneNormal, SeqToOneTDist
 
 include("./netpriors/abstract.jl")
 include("./netpriors/gaussian.jl")
+export NetworkPrior, sample_prior 
+export GaussianPrior
 
 include("./initialisers/abstract.jl")
 include("./initialisers/basics.jl")
+export BNNInitialiser
+export InitialiseAllSame
 
 include("./model/posterior.jl")
 include("./layers/dense.jl")
@@ -26,21 +38,6 @@ include("./simulations/AR.jl")
 include("./utils/rnn_utils.jl")
 
 ###### Exports
-export destruct
-
-export BNNLikelihood, predict
-export FeedforwardNormal, FeedforwardTDist
-export SeqToOneNormal, SeqToOneTDist
-
-export NetworkPrior, sample_prior 
-export GaussianPrior
-
-export BNNInitialiser
-export InitialiseAllSame
-
-
-
-export BNN, BLayer
 export posterior_predict
 export lp, reconstruct_sample
 export find_mode, find_mode_sgd
