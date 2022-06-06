d9df60213544fef1e82a67b6df33a16bed5c6d42
Enrico Wegner <wegner1996@gmail.com>
2022-06-06 11:00:20 +0200

    allowing for mass adaptation in SGNHT-S

--- src/inference/mcmc/adapters/mass/fixedmassmatrix.jl
+++ src/inference/mcmc/adapters/mass/fixedmassmatrix.jl
@@ -2,11 +2,11 @@ using LinearAlgebra
 """
 Use a fixed inverse mass matrix.
 """
-struct FixedMassAdapter <: MassAdapter 
+mutable struct FixedMassAdapter <: MassAdapter 
     Minv::AbstractMatrix
 end
 FixedMassAdapter() = FixedMassAdapter(Matrix(undef, 0, 0))
-function (madapter::FixedMassAdapter)(s::MCMCState, θ::AbstractMatrix{T}, bnn, ∇θ) where {T}
+function (madapter::FixedMassAdapter)(s::MCMCState, θ::AbstractVector{T}, bnn, ∇θ) where {T}
     size(madapter.Minv, 1) == 0 && (madapter.Minv = Diagonal(one.(θ)))
     return madapter.Minv
 end
