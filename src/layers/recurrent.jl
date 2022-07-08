2908d912d8592a2f2bf35ea6d8d20376e7376b06
Enrico Wegner <wegner1996@gmail.com>
2022-07-08 11:44:14 +0200

    destruct lstm was gone

--- src/layers/recurrent.jl
+++ src/layers/recurrent.jl
@@ -32,3 +32,37 @@ function destruct(cell::Flux.Recur{R}) where {R<:Flux.RNNCell}
     end
     return θ, re
 end
+
+
+################################################################################
+# LSTM
+################################################################################
+
+function destruct(cell::Flux.Recur{R}) where {R<:Flux.LSTMCell}
+    @unpack Wi, Wh, b, state0 = cell.cell
+    # state 0 has two states, one for h and one for c
+    # see wikipedia article
+    # θ = vcat(vec(Wi), vec(Wh), vec(b), vec(state0[1]), vec(state0[2]))
+    θ = vcat(vec(Wi), vec(Wh), vec(b))
+    function re(θ::Vector{T}) where {T}
+        s = 1
+        pWi = length(Wi)
+        new_Wi = reshape(θ[s:s+pWi-1], size(Wi))
+        s += pWi
+        pWh = length(Wh)
+        new_Wh = reshape(θ[s:s+pWh-1], size(Wh))
+        s += pWh
+        pb = length(b)
+        new_b = reshape(θ[s:s+pb-1], size(b))
+        # s += pb 
+        # pstate01 = length(state0[1])
+        # new_state01 = reshape(θ[s:s+pstate01-1], size(state0[1]))
+        new_state01 = T.(state0[1])
+        # s += pstate01 
+        # pstate02 = length(state0[2])
+        # new_state02 = reshape(θ[s:s+pstate02-1], size(state0[2]))
+        new_state02 = T.(state0[2])
+        return Flux.Recur(Flux.LSTMCell(new_Wi, new_Wh, new_b, (new_state01, new_state02)))
+    end
+    return θ, re
+end
