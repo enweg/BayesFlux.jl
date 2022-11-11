7ab3efebf8b94489382888aa7bebd2b015d194e1
Enrico Wegner <wegner1996@gmail.com>
2022-11-11 12:37:11 +0100

    testing BBB requires fewer samples

--- test/bbb.jl
+++ test/bbb.jl
@@ -1,22 +1,20 @@
 using BFlux
 using Flux, Distributions, Random
 
-function test_BBB_regression(; k=5, n=100_000)
-    k = 5
-    n = 100_000
+function test_BBB_regression(; k=5, n=10_000)
     x = randn(Float32, k, n)
     β = randn(Float32, k)
     y = x' * β + 1.0f0 * randn(Float32, n)
 
     net = Chain(Dense(5, 1))
     nc = destruct(net)
-    sigma_prior = Gamma(2.0f0, 0.5f0)
+    sigma_prior = InverseGamma(2.0f0, 0.5f0)
     like = FeedforwardNormal(nc, sigma_prior)
     prior = GaussianPrior(nc, 10.0f0)
     init = InitialiseAllSame(Normal(0.0f0, 1.0f0), like, prior)
     bnn = BNN(x, y, like, prior, init)
 
-    q, params, losses = bbb(bnn, 1000, 250; mc_samples=1, opt=Flux.RMSProp())
+    q, params, losses = bbb(bnn, 1000, 1000; mc_samples=1, opt=Flux.RMSProp(), n_samples_convergence = 1)
 
     μ = mean(q)
     test1 = maximum(abs, β - μ[1:length(β)]) < 0.05
