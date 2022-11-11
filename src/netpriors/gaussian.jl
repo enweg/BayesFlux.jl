8911f906c271389b0fa126a2b9fcad8d38a85cb5
Enrico Wegner <wegner1996@gmail.com>
2022-11-11 09:35:09 +0100

    reformatted netpriors

--- src/netpriors/gaussian.jl
+++ src/netpriors/gaussian.jl
@@ -3,9 +3,9 @@
 using Distributions
 using Random
 
-struct GaussianPrior{T, F} <: NetworkPrior
+struct GaussianPrior{T,F} <: NetworkPrior
     num_params_hyper::Int
-    nc::NetConstructor{T, F}
+    nc::NetConstructor{T,F}
     σ0::T
 end
 """
@@ -18,16 +18,16 @@ for any correlations in the network parameters in the prior.
 - `nc`: obtained using [`destruct`](@ref)
 - `σ0`: standard deviation of prior
 """
-function GaussianPrior(nc::NetConstructor{T, F}, σ0::T = T(1.0)) where {T, F}
+function GaussianPrior(nc::NetConstructor{T,F}, σ0::T=T(1.0)) where {T,F}
     return GaussianPrior(0, nc, σ0)
 end
 
-function (gp::GaussianPrior{T, F})(θnet::AbstractVector{T}, θhyper::AbstractVector{T}) where {T, F}
+function (gp::GaussianPrior{T,F})(θnet::AbstractVector{T}, θhyper::AbstractVector{T}) where {T,F}
     n = length(θnet)
-    return logpdf(MvNormal(zeros(T, n), gp.σ0^2*I), θnet)
+    return logpdf(MvNormal(zeros(T, n), gp.σ0^2 * I), θnet)
 end
 
-function sample_prior(gp::GaussianPrior{T, F}, rng::AbstractRNG = Random.GLOBAL_RNG) where {T, F}
-    θnet = rand(rng, MvNormal(zeros(T, gp.nc.num_params_network), gp.σ0^2*I))
+function sample_prior(gp::GaussianPrior{T,F}, rng::AbstractRNG=Random.GLOBAL_RNG) where {T,F}
+    θnet = rand(rng, MvNormal(zeros(T, gp.nc.num_params_network), gp.σ0^2 * I))
     return θnet
 end
--- src/netpriors/mixturescale.jl
+++ src/netpriors/mixturescale.jl
@@ -15,24 +15,24 @@ Scale mixture of Gaussians
 - `π1`: Weight/Probability of first Gaussian 
 - `π2`: Weight/Probability of second Gaussian (`1-π1`)
 """
-struct MixtureScalePrior{T, F} <: NetworkPrior
+struct MixtureScalePrior{T,F} <: NetworkPrior
     num_params_hyper::Int
-    nc::NetConstructor{T, F}
+    nc::NetConstructor{T,F}
     σ1::T
     σ2::T
     π1::T
     π2::T
 end
-function MixtureScalePrior(nc::NetConstructor{T, F}, σ1::T, σ2::T, μ1::T) where {T, F}
+function MixtureScalePrior(nc::NetConstructor{T,F}, σ1::T, σ2::T, μ1::T) where {T,F}
     return MixtureScalePrior(0, nc, σ1, σ2, μ1, T(1) - μ1)
 end
 
-function (msp::MixtureScalePrior{T, F})(θnet::AbstractVector{T}, θhyper::AbstractVector{T}) where {T, F}
+function (msp::MixtureScalePrior{T,F})(θnet::AbstractVector{T}, θhyper::AbstractVector{T}) where {T,F}
     n = length(θnet)
-    return msp.π1 * logpdf(MvNormal(zeros(T, n), msp.σ1^2*I), θnet) + msp.π2 * logpdf(MvNormal(zeros(T, n), msp.σ2^2*I), θnet)
+    return msp.π1 * logpdf(MvNormal(zeros(T, n), msp.σ1^2 * I), θnet) + msp.π2 * logpdf(MvNormal(zeros(T, n), msp.σ2^2 * I), θnet)
 end
 
-function sample_prior(msp::MixtureScalePrior{T, F}, rng::AbstractRNG = Random.GLOBAL_RNG) where {T, F}
+function sample_prior(msp::MixtureScalePrior{T,F}, rng::AbstractRNG=Random.GLOBAL_RNG) where {T,F}
     n = msp.nc.num_params_network
     θnet1 = rand(rng, MvNormal(zeros(T, n), msp.σ1^2 * I))
     θnet2 = rand(rng, MvNormal(zeros(T, n), msp.σ2^2 * I))
