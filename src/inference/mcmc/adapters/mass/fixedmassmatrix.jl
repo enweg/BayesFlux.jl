9b78659d1f921cc1d37b62544bd2c1265b96b274
Enrico Wegner <wegner1996@gmail.com>
2022-11-11 10:23:03 +0100

    putting space around functions

--- src/inference/mcmc/adapters/mass/fixedmassmatrix.jl
+++ src/inference/mcmc/adapters/mass/fixedmassmatrix.jl
@@ -5,7 +5,9 @@ Use a fixed inverse mass matrix.
 mutable struct FixedMassAdapter <: MassAdapter
     Minv::AbstractMatrix
 end
+
 FixedMassAdapter() = FixedMassAdapter(Matrix(undef, 0, 0))
+
 function (madapter::FixedMassAdapter)(s::MCMCState, θ::AbstractVector{T}, bnn, ∇θ) where {T}
     size(madapter.Minv, 1) == 0 && (madapter.Minv = Diagonal(one.(θ)))
     return madapter.Minv
