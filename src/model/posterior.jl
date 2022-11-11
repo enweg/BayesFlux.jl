4178180d92308b9f381b0517be9b32222008a58a
Enrico Wegner <wegner1996@gmail.com>
2022-11-11 09:34:30 +0100

    reformatted model code

--- src/model/posterior.jl
+++ src/model/posterior.jl
@@ -1,11 +1,11 @@
 # Methods used for posterior analysis and predictions
 
-function posterior_predict(bnn::BNN, samples::Matrix{T}; kwargs ...) where {T<:Real} 
+function posterior_predict(bnn::BNN, samples::Matrix{T}; kwargs...) where {T<:Real}
     return predict(bnn, bnn.loglikelihood, samples; kwargs...)
 end
 
-function posterior_predict(bnn::BNN, samples::Array{T, 3}; kwargs...) where {T<:Real}
-    ppreds = [posterior_predict(bnn, samples[:,:,i]; kwargs...) for i=1:size(samples, 3)]
-    ppreds = cat(ppreds...; dims = 3)
+function posterior_predict(bnn::BNN, samples::Array{T,3}; kwargs...) where {T<:Real}
+    ppreds = [posterior_predict(bnn, samples[:, :, i]; kwargs...) for i = 1:size(samples, 3)]
+    ppreds = cat(ppreds...; dims=3)
     return ppreds
 end
